8455ab4cf66c469fa7f314c24a5e374c43a6a917
mee1b <mee1b@yandex.ru>
2025-04-23 12:43:48 +0900

    Initial commit

---
+++ ChoiceSort/ChoiceSort/ChoiceSort.cpp
@@ -0,0 +1,58 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <list>
+#include <iterator>
+#include <future>
+#include <thread>
+
+static int findMinimum(std::list<int>& l) 
+{
+    return *std::min_element(begin(l), end(l));
+}
+
+static void choiceSort(std::list<int>& l, std::promise<std::string> value) 
+{
+    for (int j = 0; j < l.size(); j++) 
+    {
+        auto begin = std::next(l.begin(), j);
+        auto end = l.end();
+        std::list<int> subList;
+        subList.splice(subList.begin(), l, begin, end);
+
+        std::future<int> result(std::async(std::launch::async, findMinimum, ref(subList)));
+        int m = result.get();
+
+        if (m != *subList.begin()) 
+        {
+            subList.insert(std::find(subList.begin(), subList.end(), m), *subList.begin());
+            subList.pop_front();
+            subList.remove(m);
+            subList.push_front(m);
+        }
+        l.splice(end, subList);
+    }
+    value.set_value("Sorting is completed");
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+}
+
+static void printList(std::list<int>& l) 
+{
+    for (const auto& elem : l) std::cout << elem << " ";
+    std::cout << std::endl;
+}
+
+int main()
+{
+
+    std::list<int> list1 = { 2, 7, 5, 17, 26, 32, 1, 4, 8 };
+    std::promise<std::string> value;
+    std::future<std::string> valueFuture = value.get_future();
+
+    std::thread Asyncthread1(std::move(choiceSort), std::ref(list1), std::move(value));
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    std::cout << valueFuture.get() << std::endl;
+
+    Asyncthread1.join();
+    printList(list1);
+    return 0;
+}
---
+++ for_each/for_each/for_each.cpp
@@ -0,0 +1,47 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <list>
+#include <iterator>
+#include <future>
+#include <thread>
+
+typedef unsigned int ui;
+
+static int increment_vector(int value) 
+{
+	return value * 2;
+}
+
+template<typename It, typename Func>
+void async_for_each(It first, It last, Func f) 
+{
+	const ui length = std::distance(first, last);
+	if (!length)
+		return;
+	const ui min_per_thread = 10;
+	if (length < (2 * min_per_thread)) 
+	{
+		std::for_each(first, last, f);
+	}
+	else 
+	{
+		It const mid_point = first + length / 2;
+		std::future<void> first_half = std::async(&async_for_each<It, Func>, first, mid_point, f);
+		std::future<void> last_half = std::async(&async_for_each<It, Func>, mid_point, last, f);
+		first_half.get();
+		last_half.get();
+	}
+}
+static void printVector(std::vector<int>& l)
+{
+	for (const auto& elem : l) std::cout << elem << " ";
+	std::cout << std::endl;
+}
+int main() 
+{
+	std::vector<int> v{ 1,4,6,9,12,20 };
+	printVector(v);
+	async_for_each(v.begin(), v.end(), [](int value) {std::cout << increment_vector(value) << " "; });
+
+	return 0;
+}
